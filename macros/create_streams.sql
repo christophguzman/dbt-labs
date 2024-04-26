32da4b1698bf1a320ba9b08de11ad0c3727de5c2
Christopher Guzman <christopher.m.guzman@accenture.com>
2024-04-26 16:16:44 +0000

    Fixed create_streams

--- macros/create_streams.sql
+++ macros/create_streams.sql
@@ -1,6 +1,10 @@
 {% macro create_streams(macro_name) -%}
     {%- set sql -%}
         
+        USE DATABASE {{target.database}};
+        
+        USE SCHEMA {{generate_schema_name('MARTS')}};
+
         CREATE OR REPLACE STREAM {{ macro_name }}_STREAM ON TABLE {{ macro_name }} ;
         
     {%- endset -%}
